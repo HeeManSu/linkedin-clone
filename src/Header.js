af919a33b742e4974bd483b6f97c19f18f67c438
Himanshu <himanshu.kumarr07@gmail.com>
2022-09-12 13:17:38 +0530

    First commit

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <div className="header__left">
+                <img src='' alt='' />
+                <div className="header__search">
+                    {/* SearchIcon */}
+                    <input type="text" />
+                </div>
+            </div>
+            <div className="header__right">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header
